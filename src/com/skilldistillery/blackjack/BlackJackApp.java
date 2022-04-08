a35b42d8a6de2e2ee5661b891e3b549e753c5ef4
Angel <angel1179008@gmail.com>
2022-04-08 13:26:57 -0600

    pushed card, deck, rank, suit

---
+++ src/com/skilldistillery/blackjack/BlackJackApp.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.blackjack;
+
+public class BlackJackApp {
+	public static void main(String[] args) {
+		BlackJackApp app = new BlackJackApp();
+		app.run();
+		
+	}
+	public void run() {
+		
+	}
+	
+
+}
